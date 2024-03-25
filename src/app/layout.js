b4752665e2a5dc4be4ea26b18e16ef46fea9a5ef
saadafahmed45 <hafijnahid45@.com>
2024-03-26 02:21:10 +0600

    add

--- src/app/layout.js
+++ src/app/layout.js
@@ -3,6 +3,7 @@ import "./globals.css";
 import Navbar from "./components/Navbar";
 import Footer from "./components/Footer";
 import ContextProvider from "./Context/Context";
+import { SpeedInsights } from "@vercel/speed-insights/next";
 
 const inter = Inter({ subsets: ["latin"] });
 
