e4948aa30ecc4cf807cfc372739bfb2a2eab2c66
saadafahmed45 <hafijnahid45@.com>
2024-03-26 02:24:02 +0600

    add

--- src/app/layout.js
+++ src/app/layout.js
@@ -21,6 +21,7 @@ export default function RootLayout({ children }) {
           {children}
           <Footer />
         </ContextProvider>
+        <SpeedInsights />
       </body>
     </html>
   );
